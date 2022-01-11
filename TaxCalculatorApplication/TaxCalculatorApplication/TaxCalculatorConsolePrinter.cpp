00544e7552286247768dc68abd6446bcbcf070d9
Hossein Karimi <h.karimi.arjomand@gmail.com>
2022-01-11 21:46:23 +0100

    finish TaxCalculatorConsolePrinter

--- TaxCalculatorApplication/TaxCalculatorApplication/TaxCalculatorConsolePrinter.cpp
+++ TaxCalculatorApplication/TaxCalculatorApplication/TaxCalculatorConsolePrinter.cpp
@@ -6,10 +6,18 @@ using std::cout, std::endl;
 
 void TaxCalculatorConsolePrinter::printReceipt(const ShoppingBasket& shoppingBasket) const
 {
-
+	double salesTaxes = 0;
+	double total = 0;
+	for (const Item& item : shoppingBasket)
+	{
+		cout << item.amount() << (item.imported() ? " imported " : " ") << item.name() << ": " << item.grossPrice() << endl;
+		salesTaxes += item.amount() * (item.grossPrice() - item.netPrice());
+		total += item.amount() * item.grossPrice();
+	}
+	cout << "Sales Taxes: " << salesTaxes << "\nTotal: " << total << endl << endl;
 }
 
 void TaxCalculatorConsolePrinter::printError(std::string_view error) const
 {
-
+	std::cerr << error << endl << endl;
 }
