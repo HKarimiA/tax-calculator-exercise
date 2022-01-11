128daf1e077de442083fc40cc0cbe28cafcf9c61
Hossein Karimi <h.karimi.arjomand@gmail.com>
2022-01-11 19:12:24 +0100

    add ShoppingBasketFromTextReaderTest

---
+++ TaxCalculatorApplication/TaxCalculatorTest/ShoppingBasketFromTextReaderTest.cpp
@@ -0,0 +1,17 @@
+#include "CppUnitTest.h"
+#include "../TaxCalculatorApplication/ShoppingBasketFromTextReader.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TaxCalculatorTest
+{
+	TEST_CLASS(ShoppingBasketFromTextReaderTest)
+	{
+	public:
+		TEST_METHOD(EvaluateTaxOfItem)
+		{
+
+		}
+	};
+}
