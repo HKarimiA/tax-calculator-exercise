ec5ac0e058475fbc5b3fdc362a7487c88273433b
Hossein Karimi <h.karimi.arjomand@gmail.com>
2022-01-11 19:20:49 +0100

    test ShoppingBasketFromTextReader::getShoppingBasketName

--- TaxCalculatorApplication/TaxCalculatorTest/ShoppingBasketFromTextReaderTest.cpp
+++ TaxCalculatorApplication/TaxCalculatorTest/ShoppingBasketFromTextReaderTest.cpp
@@ -1,6 +1,6 @@
 #include "CppUnitTest.h"
 #include "../TaxCalculatorApplication/ShoppingBasketFromTextReader.h"
-
+#include "../TaxCalculatorApplication/ItemsTaxFileReader.h"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,6 +9,13 @@ namespace TaxCalculatorTest
 	TEST_CLASS(ShoppingBasketFromTextReaderTest)
 	{
 	public:
+		TEST_METHOD(GetShoppingBasketName)
+		{
+			std::unique_ptr<ItemsTaxReader> itemsTaxFileReader = std::make_unique<ItemsTaxFileReader>();
+			ShoppingBasketFromTextReader s(itemsTaxFileReader, "../TaxCalculatorApplication/input1.txt");
+			Assert::AreEqual(s.getShoppingBasketName(), std::string("../TaxCalculatorApplication/input1.txt"));
+		}
+
 		TEST_METHOD(EvaluateTaxOfItem)
 		{
 
